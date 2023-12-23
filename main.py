b1dfa89e36fb6c0faa10a3afd81dfa6f113007e0
datascienceanywhere <datascienceanywhere@gmailcom>
2023-12-23 12:52:53 +0530

    rtc configuration

--- main.py
+++ main.py
@@ -14,4 +14,7 @@ def video_frame_callback(frame):
     return av.VideoFrame.from_ndarray(flipped, format="bgr24")
 
 
-webrtc_streamer(key="example", video_frame_callback=video_frame_callback)
+webrtc_streamer(key="example", video_frame_callback=video_frame_callback,
+                rtc_configuration={  # Add this config
+                    "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
+                )
