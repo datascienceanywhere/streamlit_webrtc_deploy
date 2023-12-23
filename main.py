3adc0b27330cc23a61e476bdde543ce416107b6c
datascienceanywhere <datascienceanywhere@gmailcom>
2023-12-23 15:09:18 +0530

    remove media stream

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
 import streamlit as st
 from streamlit_webrtc import webrtc_streamer
 import av
-import os
-from twilio.rest import Client
 
-# Find your Account SID and Auth Token at twilio.com/console
-# and set the environment variables. See http://twil.io/secure
-account_sid = os.environ['TWILIO_ACCOUNT_SID']
-auth_token = os.environ['TWILIO_AUTH_TOKEN']
-client = Client(account_sid, auth_token)
-
-token = client.tokens.create()
 
 flip = st.checkbox("Flip")
 
@@ -20,11 +11,12 @@ def video_frame_callback(frame):
 
     flipped = img[::-1,:,:] if flip else img
 
-    return av.VideoFrame.from_ndarray(flipped, format="bgr24")
+    return frame#av.VideoFrame.from_ndarray(flipped, format="bgr24")
 
 
 webrtc_streamer(key="example", video_frame_callback=video_frame_callback,
                 rtc_configuration={  # Add this config
-                    "iceServers": token.ice_servers
-                }
+                    "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
+                },
+                media_stream_constraints={"video": True, "audio": False}
                 )
