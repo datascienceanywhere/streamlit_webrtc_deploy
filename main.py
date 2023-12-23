31f396e91241efdd6c20559bf529ce7035979ac0
datascienceanywhere <datascienceanywhere@gmailcom>
2023-12-23 13:25:27 +0530

    twilio

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 import streamlit as st
 from streamlit_webrtc import webrtc_streamer
 import av
+import os
+from twilio.rest import Client
 
+# Find your Account SID and Auth Token at twilio.com/console
+# and set the environment variables. See http://twil.io/secure
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+client = Client(account_sid, auth_token)
+
+token = client.tokens.create()
 
 flip = st.checkbox("Flip")
 
 
 def video_frame_callback(frame):
-    # img = frame.to_ndarray(format="bgr24")
+    img = frame.to_ndarray(format="bgr24")
 
-    # flipped = img[::-1,:,:] if flip else img
+    flipped = img[::-1,:,:] if flip else img
 
-    return frame#av.VideoFrame.from_ndarray(flipped, format="bgr24")
+    return av.VideoFrame.from_ndarray(flipped, format="bgr24")
 
 
 webrtc_streamer(key="example", video_frame_callback=video_frame_callback,
                 rtc_configuration={  # Add this config
-                    "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
+                    "iceServers": token.ice_servers
                 }
                 )
